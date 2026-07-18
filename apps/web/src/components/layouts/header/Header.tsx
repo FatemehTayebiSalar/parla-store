1b2725585f885054fbcdb9b615ba906886a9226c
FatemehTayebiSalar <fatemehtayebi98@gmail.com>
2026-07-18 22:04:21 +0330

    refactor: extract reusable Container component

--- apps/web/src/components/layouts/header/Header.tsx
+++ apps/web/src/components/layouts/header/Header.tsx
@@ -1,17 +1,21 @@
-import HeaderActions from "./HeaderActions"
-import Logo from "./Logo"
-import SearchBar from "./SearchBar"
+import HeaderActions from "./HeaderActions";
+import Logo from "./Logo";
+import SearchBar from "./SearchBar";
 import Navigation from "../navigation";
+import Container from "../../common/Container";
+
 function Header() {
   return (
     <header className="border-b bg-white">
-      <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-6">
-        <HeaderActions />
-        <SearchBar />
-        <Logo />
-      </div>
+      <Container>
+        <div className="flex h-20 items-center justify-between">
+          <HeaderActions />
+          <SearchBar />
+          <Logo />
+        </div>
+      </Container>
+
       <Navigation />
-      
     </header>
   );
 }
