5a192d2d486cb1fb16516d6ebce699d0888b6c21
FatemehTayebiSalar <fatemehtayebi98@gmail.com>
2026-07-18 22:42:59 +0330

    style: add initial footer styles

--- apps/web/src/components/layouts/footer/Footer.tsx
+++ apps/web/src/components/layouts/footer/Footer.tsx
@@ -3,11 +3,14 @@ import { Logo } from "../header";
 
 function Footer() {
   return (
-     <footer>
+     <footer className="border-t bg-white py-10">
         <Container>
-            <Logo />
-            <p>© 2026 PARLA Clean Shop. All rights reserved.</p>
-            
+            <div className="flex flex-col items-center gap-4">
+                <Logo />
+                <p className="text-sm text-gray-500">
+                    © 2026 PARLA Clean Shop. All rights reserved.
+                </p>
+            </div>
         </Container>
     </footer> 
   
